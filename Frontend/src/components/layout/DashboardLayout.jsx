9d236c5c01225c353db08d913ab23a3052fbf16e
bhavesh <bhaveshbhat2003@gmail.com>
2026-06-06 10:17:19 +0530

    Add Layout Components in react

---
+++ Frontend/src/components/layout/DashboardLayout.jsx
@@ -0,0 +1,18 @@
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+
+      <div className="flex">
+        <Sidebar />
+
+        <main className="flex-1 p-8">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
