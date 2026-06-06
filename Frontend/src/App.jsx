c56a379256c2e2d1a378c46d9573203f43a475b1
bhavesh <bhaveshbhat2003@gmail.com>
2026-06-06 10:51:46 +0530

    Install few dependancies, set up routes in react

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CompanyInfo from "./pages/recruiter/CompanyInfo";
+import FoundingInfo from "./pages/recruiter/FoundingInfo";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CompanyInfo />} />
+        <Route path="/recruiter/founding-info" element={<FoundingInfo />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;44
+export default App;
