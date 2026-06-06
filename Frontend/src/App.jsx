2404a6a58fb4165b2f04c9fc47d1b315c8b1bbab
abhii1807 <abhikhsingh321@gmail.com>
2026-06-06 15:22:23 +0530

    added user auth(login,Signupand Email-Verifiaction) pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,12 +4,30 @@ import FoundingInfo from "./pages/recruiter/FoundingInfo";
 import SocialMediaProfile from "./pages/recruiter/SocialMediaProfile";
 import ContactInfo from "./pages/recruiter/ContactInfo";
 import ProfileComplete from "./pages/recruiter/ProfileComplete";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import VerifyEmail from "./pages/auth/VerifyEmail";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/recruiter/company-info" />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/candidate/dashboard"
+          element={<h1 className="text-3xl font-bold">Candidate Dashboard</h1>}
+        />
+
+        <Route
+          path="/recruiter/dashboard"
+          element={<h1 className="text-3xl font-bold">Recruiter Dashboard</h1>}
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        {/* modify it when you are working on recruiter */}
+        {/* <Route path="/" element={<Navigate to="/recruiter/company-info" />} /> */}
 
         <Route path="/recruiter/company-info" element={<CompanyInfo />} />
         <Route path="/recruiter/founding-info" element={<FoundingInfo />} />
