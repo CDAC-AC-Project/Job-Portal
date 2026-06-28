0b40a5feeb7800d699664be61f9bb8981d47d3e8
bhavesh <bhaveshbhat2003@gmail.com>
2026-06-28 22:39:41 +0530

    Implement AuthController and UserRepository

--- Backend/Auth_User-Service/src/main/java/com/cdac/daos/UserRepository.java
+++ Backend/Auth_User-Service/src/main/java/com/cdac/daos/UserRepository.java
@@ -1,5 +1,14 @@
 package com.cdac.daos;
 
-public interface UserRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cdac.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
