08efa8527fa74a41cd4c995a349867ed5e696a1d
bhaveshbhat09 <126270098+bhaveshbhat09@users.noreply.github.com>
2026-07-29 19:37:41 +0530

    feat: add API Gateway

---
+++ Backend/ApiGateway/src/main/java/com/jobportal/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.jobportal.config;
+
+import com.jobportal.filter.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    	http.csrf(csrf -> csrf.disable());
+		// 2. Retain basic auth scheme (disable form based auth)
+	//	http.httpBasic(Customizer.withDefaults());
+		// 3. Disable HttpSession (Tell Spring sec - DO NOT create HttpSession object
+		// to store Spring security context holder
+		http.sessionManagement(session -> 
+		session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		// 4. Add a rule - all endpoints - secured (requires Authentication)
+		http.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/login").permitAll()
+				.requestMatchers("/register").permitAll()
+				.requestMatchers("/actuator/health").permitAll()
+				.requestMatchers(HttpMethod.GET,"/products/**").hasAnyRole("CUSTOMER","ADMIN")
+				.requestMatchers(HttpMethod.POST,"/products/**").hasRole("ADMIN")
+				.anyRequest().authenticated()
+				)
+
+			.addFilterBefore(jwtAuthenticationFilter,
+				UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+
+            config.setAllowedOrigins(List.of("http://localhost:5173"));
+            config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+            config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+            config.setExposedHeaders(List.of("Authorization"));
+            config.setAllowCredentials(true);
+
+            return config;
+        };
+    }
+}
