236dbda4ca870cfaadb567c7d4fd626f0e975921
abhii1807 <abhikhsingh321@gmail.com>
2026-08-07 09:21:01 +0530

    feat:removed smtp credentials

--- Frontend/src/components/recruiter/settings/CompanyInfoSettings.jsx
+++ Frontend/src/components/recruiter/settings/CompanyInfoSettings.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { FiUploadCloud } from "react-icons/fi";
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 // import {
 //   FiUploadCloud,
 //   FiX,
